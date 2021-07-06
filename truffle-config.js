074d1fa5b5a7a6d13188e2e2eaa81fa84b6555c0
liyang-ust <ylikp@connect.ust.hk>
2021-07-06 11:30:48 +0800

    petshop demo

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,14 @@
+var HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const dotenv = require('dotenv');
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
+console.log(result.parsed);
+
+var mnemonic_ropsten = process.env.mnemonic_ropsten;
+var infura_apikey = '';
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +20,19 @@ module.exports = {
     },
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic_ropsten, 'https://ropsten.infura.io/v3/' + infura_apikey),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true  
+    }
+  },
+  compilers: {
+    solc:{
+      version: '0.5.6'
     }
   }
 };
